cd5c315698b22a53c80f76083d0948449d4506a8
tanvirtin <tanvir.tinz@gmail.com>
2017-11-06 03:28:49 -0500

    skeleton created

---
+++ HopfieldNetwork.py
@@ -0,0 +1,20 @@
+class HopfieldNetwork(object):
+	def __init__(self):
+		pass
+
+	def feed(self):
+		pass
+
+	def train(self):
+		pass
+
+	def activationFunction(self):
+		pass
+
+
+def main():
+	pass
+
+
+if __name__ == "__main__":
+	main()
